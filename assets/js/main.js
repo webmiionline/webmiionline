15d812b56d48397ec1a7867ba1a365b9185adc67
webmiionline <WEBMII@W33D.ORG>
2024-09-16 00:09:54 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,8 @@
-
-
 // UI audio
 function hover(){
 	var audio = document.getElementById("hover");
 	audio.play();
 }
-
 
 // click audio
 function select(){
@@ -37,36 +34,40 @@ day = d.getDate();
 month = d.getMonth() + 1;
 date = weekday + " " + month + "/" + day;
 
-$( document ).ready(function() {
+// delay function
+function delay(callback, ms) {
+  setTimeout(callback, ms);
+}
+
+$(document).ready(function() {
   // go to main menu when channel is clicked
 	$("body").on("click", ".occupied .hover", function(){
 		var centerX = $(this).offset().left + $(this).width() / 2;
 		var centerY = $(this).offset().top + $(this).height() / 2;
-		$( ".main-menu" ).css( {"transform-origin" : centerX + "px " + centerY + "px 0px"} );
+		$(".main-menu").css({"transform-origin" : centerX + "px " + centerY + "px 0px"});
 
-    var img = $( this ).attr( "data-img" );
-		$( ".splash-screen" ).css( {"background-image" : " url(" + img + ")", "transform-origin" : centerX + "px " + centerY + "px 0px"} );
+		var img = $(this).attr("data-img");
+		$(".splash-screen").css({"background-image" : "url(" + img + ")", "transform-origin" : centerX + "px " + centerY + "px 0px"});
 
-		$( ".main-menu" ).addClass( "channel-splash" );
-		$( "body" ).addClass( "channel-splash" );
+		$(".main-menu").addClass("channel-splash");
+		$("body").addClass("channel-splash");
 		delay(function(){
-			$( "body" ).removeClass( "splash-switch" );
-		}, 900 );
+			$("body").removeClass("splash-switch");
+		}, 900);
 	});
 
 	// back to main menu
 	$("body").on("click", ".menu-btn", function(){
-		$( ".main-menu" ).removeClass( "channel-splash" );
-		$( "body" ).removeClass( "channel-splash" );
-		$( "body" ).addClass( "splash-switch" );
+		$(".main-menu").removeClass("channel-splash");
+		$("body").removeClass("channel-splash");
+		$("body").addClass("splash-switch");
 		delay(function(){
-			$( "body" ).removeClass( "splash-switch" );
-		}, 900 );
+			$("body").removeClass("splash-switch");
+		}, 900);
 	});
 
 	// ignore screen warning
 	$("body").on("click", ".screen-message", function(){
-		$( ".screen-message" ).addClass( "hidden" );
+		$(".screen-message").addClass("hidden");
 	});
 });
-
